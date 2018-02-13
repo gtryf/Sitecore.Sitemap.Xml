23e66afdd65ca7448f93eb9f3bd4cbb716eda15e
George Tryfonas <barbageorge@hotmail.com>
2018-02-13 17:40:36 +0200

    Add sitemap rebuild agent

---
+++ Sitemap.Xml/Agents/BuildSitemapAgent.cs
@@ -0,0 +1,14 @@
+﻿using LD.Sitemap.Xml.Events;
+using System;
+
+namespace LD.Sitemap.Xml.Agents
+{
+    public class BuildSitemapAgent
+    {
+        public void Run()
+        {
+            Sitecore.Events.Event.RaiseEvent("sitemap:rebuild", new EventArgs());
+            Sitecore.Eventing.EventManager.QueueEvent(new BuildSitemapRemoteEvent());
+        }
+    }
+}
